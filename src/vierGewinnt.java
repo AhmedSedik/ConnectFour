24bbbfee90f62d98c55b033c2632ffb4e085b905
Yasseen <Yasseen@DESKTOP-RG1EJ91>
2019-11-01 01:42:56 +0100

    checkGewinner Completed

--- src/vierGewinnt.java
+++ src/vierGewinnt.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class vierGewinnt extends Spiel implements Protokollierbar {
 
 	private static Spieler currentSpieler; 
-
-
+	private static Spieler spieler1;
+	private static Spieler spieler2;
+	
 
     public static void main(String args[]) {
-        Spieler spieler1,spieler2;
         Scanner scanner = new Scanner(System.in);
         int x ,y;
         x = scanner.nextInt();
@@ -30,51 +30,94 @@ public class vierGewinnt extends Spiel implements Protokollierbar {
         spieler1 = new Spieler(farbeS1, false);
         spieler2 = new Spieler(farbeS2, false);
         currentSpieler = spieler1; 
-        
+     
+        while (true) {   
+ 
         System.out.println("Please enter column number");
         Scanner scanner2 = new Scanner(System.in);
         int r = scanner.nextInt() - 1;
         add2oshata(board,r,currentSpieler);
         board.feldDarestellung(board.getSpielfeld());
-   
-        
-       
-       // while (true) {
-        	
-        	
-        	
-        
-
+        if(checkGewinner(board)) {
+        	System.out.println(currentSpieler.getName() + " HAS WON!!");
+        	break;
+        }
+        spielerWechseln();
+     }
+    
     }
-
-    public static boolean add2oshata (Spielfeld spielfeld, int spalte, Spieler currentSpieler ) {
-    	
+    
+    public static void spielerWechseln() {
+    	if (currentSpieler == spieler1)
+    		currentSpieler = spieler2;
+    		else 
+    			currentSpieler = spieler1;
+    			
+    }
+    
+    public static boolean add2oshata (Spielfeld spielfeld, int spalte, Spieler currentSpieler ) { 	
     	if (spielfeld.getSpielfeld()[0][0] != '☐') {
     		System.out.println("Die Spalte ist voll!");
     		return false;
     	}
-    	
-    	for(int reihe = spielfeld.getRows() - 1; reihe >= 0; reihe --) {
-    		
+
+    	for(int reihe = spielfeld.getRows() - 1; reihe >= 0; reihe --) {    		
     		if(spielfeld.getSpielfeld()[reihe][spalte] == '☐') {
     				spielfeld.getSpielfeld()[reihe][spalte] = currentSpieler.getName();
-    				System.out.println("hi");
     				return true; 
     		}
     	}
     	return true;
     }
     
-    public boolean checkGewinner () {
-    	return true;
+    public static boolean checkGewinner (Spielfeld spielfeld) {
+    	
+    	//horizontal
+    	for (int row = 0; row < spielfeld.getSpielfeld().length; row++){
+    		for (int col = 0; col < spielfeld.getSpielfeld()[row].length - 3; col++){
+    			if (spielfeld.getSpielfeld()[row][col] != '☐' && spielfeld.getSpielfeld()[row][col] == spielfeld.getSpielfeld()[row][col+1] 
+    				&& spielfeld.getSpielfeld()[row][col] == spielfeld.getSpielfeld()[row][col+2] 
+    				&& spielfeld.getSpielfeld()[row][col] == spielfeld.getSpielfeld()[row][col+3])
+    				return true;
+    		}
+    	}
+    	
+    	//vertical
+    	for (int col = 0; col < spielfeld.getSpielfeld()[0].length; col++){
+    		for (int row = 0; row < spielfeld.getSpielfeld().length - 3; row++){
+    			if (spielfeld.getSpielfeld()[row][col] != '☐' && spielfeld.getSpielfeld()[row][col] == spielfeld.getSpielfeld()[row+1][col]
+    				&& spielfeld.getSpielfeld()[row][col] == spielfeld.getSpielfeld()[row+2][col] 
+    				&& spielfeld.getSpielfeld()[row][col] == spielfeld.getSpielfeld()[row+3][col])
+    				return true;
+    		}
+    	}
+    	
+    	//diagonal oben links
+    	for (int row = 0; row < spielfeld.getSpielfeld().length - 3; row++){
+    		for (int col = 0; col < spielfeld.getSpielfeld()[row].length - 3; col++){
+    			if (spielfeld.getSpielfeld()[row][col] != '☐' && spielfeld.getSpielfeld()[row][col] == spielfeld.getSpielfeld()[row+1][col+1] 
+    				&& spielfeld.getSpielfeld()[row][col] == spielfeld.getSpielfeld()[row+2][col+2]
+    				&& spielfeld.getSpielfeld()[row][col] == spielfeld.getSpielfeld()[row+3][col+3])
+    				return true;
+    		}
+    	}
+    	
+    	//diagonal oben rechts
+    	for (int row = 0; row < spielfeld.getSpielfeld().length - 3; row++){
+    		for (int col = 3; col < spielfeld.getSpielfeld()[row].length; col++){
+    			if (spielfeld.getSpielfeld()[row][col] != '☐' && spielfeld.getSpielfeld()[row][col] == spielfeld.getSpielfeld()[row+1][col-1] 
+    				&& spielfeld.getSpielfeld()[row][col] == spielfeld.getSpielfeld()[row+2][col-2] 
+    				&& spielfeld.getSpielfeld()[row][col] == spielfeld.getSpielfeld()[row+3][col-3])
+    		return true;
+    		}
+    	}
+    	return false;
     }
     
     @Override
     public void add(int lastCol, int lastRow, Spieler currentSpieler) {
-
-
     }
-
+    
     @Override
     public void remove(int lastCol, int lastRow, Spielfeld speilfeld) {
 
